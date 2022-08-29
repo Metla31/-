b0e137ba3852124d3f55df4dbbe939a674f3966b
Metla31 <Metlaiceblade@yandex.ru>
2022-08-29 21:52:34 +0300

    form validation

--- source/js/main.js
+++ source/js/main.js
@@ -6,7 +6,7 @@ import {initModals} from './modules/modals/init-modals';
 window.addEventListener('DOMContentLoaded', () => {
 
   // Utils
-
+  // Мобильное меню
   const openMenuButton = document.querySelector('.page-header__toggle');
   const closeMenuButton = document.querySelector('.mobile-nav__toggle');
   const menuOpen = document.querySelector('.mobile-nav');
@@ -26,6 +26,42 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     });
   }
+
+  // Валидация формы
+
+  const form = document.querySelector('.form');
+  const fields = form.querySelectorAll('.field');
+
+  const generateError = function (text) {
+    const error = document.createElement('div');
+    error.className = 'error';
+    error.style.color = 'red';
+    error.innerHTML = text;
+    return error;
+  };
+
+  const removeValidation = function () {
+    const errors = form.querySelectorAll('.error');
+
+    for (let i = 0; i < errors.length; i++) {
+      errors[i].remove();
+    }
+  };
+
+  const checkFieldsPresence = function () {
+    for (let i = 0; i < fields.length; i++) {
+      if (!fields[i].value) {
+        const error = generateError('Заполните поле ввода');
+        form[i].parentElement.insertBefore(error, fields[i]);
+      }
+    }
+  };
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    removeValidation();
+    checkFieldsPresence();
+  });
 
 
   iosVhFix();
